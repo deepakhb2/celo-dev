3a59c1573e967d5343ee00fd01e5e83a782f3fa6
Deepak HB <deepakhb2@gmail.com>
2021-04-11 16:50:43 +0100

    Celo hello world contract.

---
+++ celo-contracts/contracts/HelloWorld.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract HelloWorld {
+  string name = 'Celo';
+
+  constructor() public {
+  }
+
+  function getName() public view returns (string memory) {
+    return name;
+  }
+
+  function setName(string calldata newName) external {
+    name = newName;
+  }
+}
